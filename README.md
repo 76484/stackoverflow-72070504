509063a8cdb6e4a606b8f02f9405575490348d10
Steven Smith <steven.76484@gmail.com>
2022-05-01 08:55:13 -0400

    Add example registering user/includes as second partial dir

---
+++ README.md
@@ -0,0 +1,5 @@
+Example solutions to Stack Overflow question: https://stackoverflow.com/q/72070504/3397771
+
+Use `npm start` to run the app.
+
+Load the page at http://localhost:3000
---
+++ server/index.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const exphbs = require("express-handlebars");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "../views", "layouts"),
+    partialsDir: [
+      path.join(__dirname, "../views", "partials"),
+      path.join(__dirname, "../views/pages/user/includes"),
+    ],
+    defaultLayout: "main",
+  })
+);
+
+app.get("/", (_req, res) => {
+  res.render("pages/user", {
+    user: {
+      firstName: "John",
+      lastName: "Doe",
+      attributes: ["Friendly", "Happy", "Smart"],
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
